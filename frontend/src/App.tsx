8d67f433ebca4ca7ce8d8dcec98a218cdad5c59e
Pablo moura <pabloclashemais@gmail.com>
2022-07-20 11:14:02 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
-  return(
+  return(<>
     <h1>Olá mundo!</h1>
+    <NotificationButton />
+    <NotificationButton />
+    </>
   )
 }
 
