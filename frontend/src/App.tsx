4d707340143ada629a80d1c1f8cf43cf53a4a7b5
Pablo moura <pabloclashemais@gmail.com>
2022-08-05 19:55:11 -0300

    Integração back e front finalizado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header"
 import SalesCard from "./components/SalesCard"
 
 function App() {
   return (<>
+  <ToastContainer />
     <Header />
     <main>
       <section id="sales">
